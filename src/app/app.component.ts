352136e3f7124b3250b0896ed123a19ec5cf71e1
Aman Chaurasia <amanchaurasia040@gmail.com>
2021-07-26 10:39:27 +0530

    AngularBasic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ecom-frontend';
+  title:string = 'Ecommerce Wonder App';
+  price:number=37383.39;
+  isMarried:boolean=true;
+
+  user={
+    id:1001,
+    name:"John",
+    age:29,
+    gender:'male'
+  }
+  private family:string[]=["John","david","Miller","Marie"];
+  imgPath="assets/img/devloper.png";
+
+  color={
+    first:'Red',
+    second:'Green',
+    third:'blue'
+  };
+
+  isError:boolean=false;
+
+  public showUserInfo(){
+    return "Id : "+this.user.id+" Name : "+this.user.name;
+  }
+
+  hasError(status:string){
+    return status ? true: false;
+  }
+
+  public changeStatus(){
+    console.log("event ",this.isMarried);
+    return this.isMarried=!(this.isMarried);
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductsComponent } from './products/products.component';
+import { UsersComponent } from './users/users.component';
+import { InvoiceComponent } from './invoice/invoice.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductsComponent,
+    UsersComponent,
+    InvoiceComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
